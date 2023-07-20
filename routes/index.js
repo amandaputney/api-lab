2b8b393ac485f903c6963701da4e2d15ed85d4f4
asp <amandasueputney@gmail.com>
2023-07-20 15:53:10 -0700

    completes set up

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chuck Norris Joke Generator' });
 });
 
 module.exports = router;
